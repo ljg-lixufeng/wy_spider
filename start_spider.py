20860bccf2a7b5fffd56c19471cfb7ee975eb0d3
xufeng <lxufeng90@163.com>
2018-09-21 14:21:32 +0800

    正常奔跑版，入队效率有点低，入队的url大多不能友好的出对提供数据

--- start_spider.py
+++ start_spider.py
@@ -13,7 +13,8 @@ class spyder():
     def __init__(self):
         self.ids_dateset, self.url_dateset, self.category_count = statistics_from_mysql()
         self.url_q = queue.Queue()
-        self.count = 0
+        self.in_count = 0
+        self.out_count = 1
         self.log = my_log("WY_spyder")
 
     # def statistics_class_num(self):
@@ -33,26 +34,25 @@ class spyder():
         for url in urls_waiting:
             self.url_q.put(url)
             c += 1
-            self.count += 1
+            self.in_count += 1
         # 如果队列大小为0，从dataset中随机挑一个给队列
         if self.url_q.qsize() == 0:
             print('qsize == 0 ，get random url from urls')
             rand = random.randint(0, len(urls))
             self.url_q.put(urls[rand])
             c += 1
-            self.count += 1
+            self.in_count += 1
         
-        if self.count % 100 == 0:
+        if self.in_count % 100 == 0:
             self.log.info(
                 'In message: 本次入队{}次，当前队列总数{}个, 解析到url个数{}，待入队个数{}，set个数{}，入队总个数{}'
-                    .format(c, self.url_q.qsize(), len(urls), len(urls_waiting), len(self.ids_dateset), self.count))
+                    .format(c, self.url_q.qsize(), len(urls), len(urls_waiting), len(self.ids_dateset), self.in_count))
 
 
 def url_out(father_url):
-    c = 0
     enq0 = threading.Thread(target=wy.urls_in, args=[father_url])
     enq0.start()
-    while True:
+    while wy.out_count%1000 != 0:
         url = wy.url_q.get()
         # 保证所爬url属于输入url的类别,http://sports.163.com
         if father_url.split('/')[2].split('.')[0] != url.split('/')[2].split('.')[0]:
@@ -60,16 +60,17 @@ def url_out(father_url):
                 enq = threading.Thread(target=wy.urls_in, args=[url])
                 enq.start()
             continue
-        # 入队操作
-        enq = threading.Thread(target=wy.urls_in, args=[url])
-        enq.start()
         # 页面是404
         try:
             if requests.get(url).status_code == 404:
+                wy.log.info('404 error')
                 continue
         except:
             print('connection error {}'.format(url))
             continue
+        # 入队操作
+        enq = threading.Thread(target=wy.urls_in, args=[url])
+        enq.start()
         # 解析并保存
         response = requests.get(url)
         try:
@@ -79,15 +80,16 @@ def url_out(father_url):
             continue
         # 跟帖小于30的，不保存
         if tcount < 100:
+            wy.log.info('tcount error: tcount:{} url: {}'.format(tcount,url))
             continue
         try:
             data2mysql(content_item, comment_items)
         except:
             wy.log.info('dateset error, url:{}'.format(url))
             continue
-        if c % 2 == 0:
-            wy.log.info("out message: 出队第{}次，跟帖：{}个，url：{}".format(c,tcount,url))
-        c += 1
+        if wy.out_count % 2 == 0:
+            wy.log.info("out message: 出队第{}次，跟帖：{}个，url：{}".format(wy.out_count,tcount,url))
+        wy.out_count += 1
 
 
 if __name__ == '__main__':
@@ -95,9 +97,6 @@ if __name__ == '__main__':
     wy.log.info(str(wy.category_count))
     category_urls = class_urls()
     while True:
-        for url in category_urls[5:]:
+        for url in category_urls[6:]:
             print('father category:%s '%(url[0]))
             url_out(url[0])
-            # 下面这两句代码根本无法执行，上面的rul_out 是个无线循环的
-            # if wy.count % 100 == 0:
-            #     continue
